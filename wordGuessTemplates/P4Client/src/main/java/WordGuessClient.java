6744c1adde1361a6d0394ad35d3764a7b4f57b3e
Ese <eomene2@uic.edu>
2020-04-28 19:40:29 -0500

    Added guessLetter method
    added a method and helper function for the GUI

--- wordGuessTemplates/P4Client/src/main/java/WordGuessClient.java
+++ wordGuessTemplates/P4Client/src/main/java/WordGuessClient.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import javafx.application.Application;
@@ -57,6 +58,7 @@ public class WordGuessClient extends Application {
 		sceneMap.put("title", titlePage());
 		sceneMap.put("start",  createStart());
 		sceneMap.put("select category", selectCategory());
+		sceneMap.put("guess letter", guessLetter());
 		sceneMap.put("listen", listenForServer());
 		sceneMap.put("game",  createGame());
 		
@@ -128,7 +130,7 @@ public class WordGuessClient extends Application {
 		return scene;
 	}
 	
-	
+	//TODO: add graphics to this
 	public Scene createStart() {
 		/*Input ip address and port number
 	
@@ -286,12 +288,12 @@ public class WordGuessClient extends Application {
 		WordInfo wordInfo = categoryRequest(category);
 		clientConnection.send(wordInfo);
 	
-		window.setScene(sceneMap.get("listen"));
+		window.setScene(sceneMap.get("guess letter"));
 		window.show();
 	
 	}
 	
-	//TODO: create a function
+	//TODO: add graphics to code
 	public Scene listenForServer(){
 		/*This scene will display until the server has
 		sent the WordInfo back to the client for the game
@@ -341,6 +343,190 @@ public class WordGuessClient extends Application {
 		//Add layout to the scene and return
 		Scene scene = new Scene(layout, WIDTH, HEIGHT);
 		return scene;
+	}
+	
+	//TODO: add graphics to code
+	public Scene guessLetter() {
+		/*This function will only have the user guess a letter.
+		We will be using 26 buttons (man this is daunting) and so we
+		will be having several helper functions to accommodate for this.
+		In case the code gets confusing, I will indent the code to let you 
+		see which node/layout belongs to which node/layout.
+			________________________________
+			|                       Lives: 3|
+			| 	                 			|
+			|								|
+			|		 Guess a Letter			|
+			|		You have _ guesses.		|
+			|								|
+			|    _ _ _ _ _ _ _ _ _ _ _ _    |
+			| 	 _______________________	|
+			|	|                		|	|
+			|	|						|	|
+			|	|						|	|
+			|	-------------------------	|
+			 ________________________________
+		 */
+		
+		//Create layout that will house all of these nodes
+		BorderPane layout = new BorderPane();
+			//This VBox will hold the information regarding the player's life
+			//and put them in the top
+			VBox topLayout = new VBox();
+				Text livesText = new Text("Lives: " + clientConnection.lives);
+				livesText.setFont(SMALLER_FONT);
+			topLayout.getChildren().add(livesText);
+			topLayout.setAlignment(Pos.TOP_RIGHT);
+			
+			//Create the centerLayout. This will tell the user the information to
+			//play, and hold the buttons the user will need to play
+			VBox centerLayout = new VBox();
+			centerLayout.setSpacing(100);
+				int guesses = clientConnection.guesses;
+				Text narrationText = new Text("Guess a letter. You have " + guesses + " guesses left.");
+				narrationText.setFont(NARRATION_FONT);
+				narrationText.setWrappingWidth(300);
+				narrationText.setTextAlignment(TextAlignment.CENTER);
+				//TODO: Find out how to get the word from the server
+				String word = "______________";	//This will change when we get the word
+				Text wordText = new Text(word);
+				wordText.setFont(NARRATION_FONT);
+				wordText.setWrappingWidth(300);
+				wordText.setTextAlignment(TextAlignment.CENTER);
+				//Create a space for the user to place their answer.
+				//This will be the 26 buttons and the confirm button.
+				HBox userInputLayout = new HBox();
+				userInputLayout.setSpacing(50);
+					//Create confirm button
+					Button confirmButton = new Button("Confirm");
+					confirmButton.setDisable(true);
+					confirmButton.setOnAction(e -> {
+						//TODO: send an updated WordInfo to the server.
+						//TODO: put a transparent waiting background on top
+						//	of the layout until the server spits back an updated
+						//	WordInfo object.
+					});
+					//Create VBox that will house the rows
+					VBox alphabetButtonLayout = new VBox();
+					alphabetButtonLayout.setSpacing(6);
+						//Create HBox that will house the buttons in each row
+						HBox row1 = new HBox();
+						HBox row2 = new HBox();
+						HBox row3 = new HBox();
+						row1.setSpacing(6);
+						row2.setSpacing(6);
+						row3.setSpacing(6);
+						row1.setAlignment(Pos.CENTER);
+						row2.setAlignment(Pos.CENTER);
+						row3.setAlignment(Pos.CENTER);
+							//Create the map with all the buttons
+							HashMap<Button, Double> buttonMap = makeAlphabetMap();
+							ArrayList<Button> row1Button = makeButtonList(buttonMap, 1.0, 2.0);
+							ArrayList<Button> row2Button = makeButtonList(buttonMap, 2.0, 3.0);
+							ArrayList<Button> row3Button = makeButtonList(buttonMap, 3.0, 4.0);
+							//Assign each button to one of three rows (much like a keyboard)
+							//and have each button perform the same action event
+							for(Button b : row1Button) {
+								b.setOnAction(e -> {
+									confirmButton.setDisable(false);
+									//TODO: Update WordInfo
+								});
+								row1.getChildren().add(b);
+							}
+							for(Button b : row2Button) {
+								b.setOnAction(e -> {
+									confirmButton.setDisable(false);
+									//TODO: Update WordInfo
+								});
+								row2.getChildren().add(b);
+							}
+							for(Button b : row3Button) {
+								b.setOnAction(e -> {
+									confirmButton.setDisable(false);
+									//TODO: Update WordInfo
+								});
+								row3.getChildren().add(b);
+							}
+					alphabetButtonLayout.getChildren().addAll(row1, row2, row3);
+					alphabetButtonLayout.setAlignment(Pos.CENTER);
+				userInputLayout.getChildren().addAll(alphabetButtonLayout, confirmButton);
+				userInputLayout.setAlignment(Pos.CENTER);
+				centerLayout.getChildren().addAll(narrationText, wordText, userInputLayout);	
+				centerLayout.setAlignment(Pos.CENTER);
+		layout.setTop(topLayout);
+		layout.setCenter(centerLayout);
+		
+		Scene scene = new Scene(layout, WIDTH, HEIGHT);
+		return scene;
+	}
+	public HashMap<Button, Double> makeAlphabetMap(){
+		//Helper function to make the alphabet buttons. They are 
+		//categorized by the row and order they would appear in on the keyboard
+		
+		HashMap<Button, Double> alphabetMap = new HashMap<Button, Double>();
+		
+		//Row 1
+		alphabetMap.put(new Button("Q"), 1.0);
+		alphabetMap.put(new Button("W"), 1.1);
+		alphabetMap.put(new Button("E"), 1.2);
+		alphabetMap.put(new Button("R"), 1.3);
+		alphabetMap.put(new Button("T"), 1.4);
+		alphabetMap.put(new Button("Y"), 1.5);
+		alphabetMap.put(new Button("U"), 1.6);
+		alphabetMap.put(new Button("I"), 1.7);
+		alphabetMap.put(new Button("O"), 1.8);
+		alphabetMap.put(new Button("P"), 1.9);
+		
+		//Row 2
+		alphabetMap.put(new Button("A"), 2.0);
+		alphabetMap.put(new Button("S"), 2.1);
+		alphabetMap.put(new Button("D"), 2.2);
+		alphabetMap.put(new Button("F"), 2.3);
+		alphabetMap.put(new Button("G"), 2.4);
+		alphabetMap.put(new Button("H"), 2.5);
+		alphabetMap.put(new Button("J"), 2.6);
+		alphabetMap.put(new Button("K"), 2.7);
+		alphabetMap.put(new Button("L"), 2.8);
+		
+		//Row 3
+		alphabetMap.put(new Button("Z"), 3.0);
+		alphabetMap.put(new Button("X"), 3.1);
+		alphabetMap.put(new Button("C"), 3.2);
+		alphabetMap.put(new Button("V"), 3.3);
+		alphabetMap.put(new Button("B"), 3.4);
+		alphabetMap.put(new Button("N"), 3.5);
+		alphabetMap.put(new Button("M"), 3.6);
+		
+		return alphabetMap;
+	}
+	public ArrayList<Button> makeButtonList(HashMap <Button, Double> map, double floor, double ceiling){
+		//Helper function that sorts the alphabet in it's proper order
+		
+		ArrayList<Button> list = new ArrayList<Button>();
+		
+		//Step 1: Iterate through hash map to find letters between threshold
+		//		  and add them to the list
+		map.forEach((button, value) -> {
+			if(value >= floor && value < ceiling) {
+				list.add(button);
+			}
+		});
+		
+		//Step 2: Sort the list to make the row for the keyboard
+		for(int i = 0; i < list.size(); i++) {
+			for(int j = i+1; j < list.size(); j++) {
+				double firstValue = map.get(list.get(i));
+				double secondValue = map.get(list.get(j));
+				if(secondValue < firstValue) {
+					Button tmp = list.get(j);
+					list.set(j, list.get(i));
+					list.set(i, tmp);
+					
+				}
+			}
+		}
+		
+		return list;
 	}
 
 	//temporary scene for testing the server
